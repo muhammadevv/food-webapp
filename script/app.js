e14e359b5f659e75468011c325fb86b83df411db
Quranabev Muhammad <mxmmad17@gmail.com>
2024-09-10 19:41:22 +0500

    first commit

--- script/app.js
+++ script/app.js
@@ -1,3 +1,9 @@
+const categoryButtons = document.querySelectorAll('.products-category_item')
+const paymentAcc = document.querySelector('.payment-accardion')
+const paymentButtons = document.querySelectorAll('.payment-button')
+const paymentBtn = document.querySelector('.payment-buttons')
+const paymentIcon = document.querySelector('.payment-icon')
+
 const swiper = new Swiper(".banner", {
   spaceBetween: 12,
   centeredSlides: false,
@@ -5,19 +11,20 @@ const swiper = new Swiper(".banner", {
   autoplay: {
     delay: 2500,
     disableOnInteraction: true,
-  },
-});
+  }
+})
 
-
-const categoryButtons = document.querySelectorAll('.products-category_item')
-
-function removeActiveAll() {
-  categoryButtons.forEach((item) =>
-    item.classList.remove('active')
-  )
-}
-
-categoryButtons.forEach((item) => item.addEventListener('click', () => {
-  removeActiveAll()
+paymentButtons.forEach((item) => item.addEventListener('click', () => {
+  paymentButtons.forEach((el) => el.classList.remove('active'))
   item.classList.add('active')
 }))
+
+categoryButtons.forEach((item) => item.addEventListener('click', () => {
+  categoryButtons.forEach((el) => el.classList.remove('active'))
+  item.classList.add('active')
+}))
+
+paymentAcc.addEventListener('click', () => {
+  paymentIcon.classList.toggle('active')
+  paymentBtn.classList.toggle('active')
+})
