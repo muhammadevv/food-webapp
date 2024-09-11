a96a4ac5c689ef7e2261e712c4ee76debad6ec32
Quranabev Muhammad <mxmmad17@gmail.com>
2024-09-11 12:08:01 +0500

    first commit

--- script/data.js
+++
@@ -1,8 +0,0 @@
-export const products = [
-  {
-    id: 1,
-    name: 'Mini Lavash Tovuqli',
-    price: 24000,
-    image: 'https://feedup.uz/_next/image?url=https%3A%2F%2Fapi.feedup.uz%2Fmedia%2Fproducts%2Fimg_11_FRpFcQS_URlBQ7I_Q6fJZmm.png&w=640&q=75'
-  }
-]
